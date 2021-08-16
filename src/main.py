b7a5332921d53ce632f41395852ab8fa5cec7870
naos080415 <chip080415@gmail.com>
2021-08-17 04:13:57 +0900

    simple-parser test

--- src/main.py
+++ src/main.py
@@ -1,8 +1,26 @@
+from html.parser import HTMLParser
 import os
+
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print("Encountered a start tag:", tag)
+
+    def handle_endtag(self, tag):
+        print("Encountered an end tag :", tag)
+
+    def handle_data(self, data):
+        print("Encountered some data  :", data)
+
+
 home_dir = os.getenv("HOME")
 import_file_path = home_dir + "/web_browser/sample_html/simple.html"
 f = open(import_file_path, 'r')
 data = f.read()
 f.close()
 
+parser = MyHTMLParser()
+
+print("*** 元のデータ ***")
 print(data)
+print("*** パース後のデータ ***")
+parser.feed(data)
